220cff0434b39f81fef74b5e4ed726be660330e7
xiaomeng li <573912623@qq.com>
2018-08-20 23:37:55 +0800

    add rpc test code

---
+++ src/RpcClient/RpcClient.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/rpc/jsonrpc"
+	"log"
+)
+
+func main()  {
+	rpcClient,err:=jsonrpc.Dial("tcp","127.0.0.1:9009")
+	if err!=nil{
+		log.Fatalln(err.Error())
+	}
+	var reply  = map[string]interface{}{}
+	var args = map[string]interface{}{}
+	args["line"]=[]int{4,5,6}
+	err=rpcClient.Call("Rpc.Handle1",args,&reply)
+	if err!=nil{
+		log.Fatalln(err.Error())
+	}
+	log.Println(reply)
+
+}
---
+++ src/gojsonrpc/RpcServer.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/rpc"
+	"net"
+	"net/rpc/jsonrpc"
+	"log"
+)
+
+type RpcFunction func(*map[string]interface{},*map[string]interface{}) 
+type Rpc struct {
+
+}
+func (this *Rpc)Handle1(args *map[string]interface{},reply *map[string]interface{})error{
+	temp:=(*args)["line"].([]interface{})
+	res:=make([]float64,0)
+	for _,v:=range temp{
+		res=append(res,v.(float64)*2)
+	}
+	*reply = map[string]interface{}{}
+	(*reply)["res"] = res
+	return nil
+}
+func main(){
+	rpchandle:=new(Rpc)
+	rpc.Register(rpchandle)
+	addr, _ := net.ResolveTCPAddr("tcp", ":9009")
+	ln, e := net.ListenTCP("tcp", addr)
+	if e != nil {
+		panic(e)
+	}
+	for {
+		conn, e := ln.Accept()
+		log.Println("[连接上了]")
+		if e != nil {
+			continue
+		}
+		go jsonrpc.ServeConn(conn)
+	}
+}
